6d096cbb85a4ce38afa53fc6506dd7effbe44986
DaheeJeong <d.hee@ewhain.net>
2024-12-22 21:12:23 +0900

    weatherType 전달(main->recommend)

--- src/pages/RecommendPage.js
+++ src/pages/RecommendPage.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect, useLocation } from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Nav from "../components/Nav";
 import styled from "styled-components";
 import banner from "../img/recommend_banner.png";
